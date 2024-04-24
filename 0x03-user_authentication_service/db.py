8449e52c16aef03581978422124ee53e615670a2
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-24 14:41:39 +0300

    PUT update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -62,11 +62,15 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """update the user by id"""
+        """Update user
+
+        Args:
+            user_id (int): id of user
+        """
         user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
+        for key, val in kwargs.items():
             if not hasattr(user, key):
                 raise ValueError
-            setattr(user, key, value)
+            setattr(user, key, val)
         self._session.commit()
         return None

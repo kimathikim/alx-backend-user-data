762a09b7d7cf525421b8b953023db4a562c023f3
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-25 00:21:36 +0300

    POST and authenticate

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask application"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 auth = Auth()
@@ -24,6 +24,21 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """login and update the session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if auth.valid_login(email, password):
+        session_id = auth.create_session(email)
+        res = jsonify({"email": f"{email}", "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+        return res
+
+    abort(401)
 
 
 if __name__ == "__main__":

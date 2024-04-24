495532c2026713765275760e411358d223fe7423
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-24 21:09:18 +0300

    update

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,21 +2,20 @@
 """
 Main file
 """
-from auth import Auth
 
-email = 'me@me.com'
-password = 'mySecuredPwd'
+from db import DB
 
-auth = Auth()
+
+my_db = DB()
+
+email = "test@test.com"
+hashed_password = "hashedPwd"
+
+user = my_db.add_user(email, hashed_password)
+print(user.id)
 
 try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
-
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
+    my_db.update_user(user.id, hashed_password="NewPwd")
+    print("Password updated")
+except ValueError:
+    print("Error")
